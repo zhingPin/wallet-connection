bdeb5bde052983fe172331aa23758bb01e5b4469
Gary Gee <crypto2doe@gmail.com>
2025-06-27 13:10:18 +0100

    added connect button functionality removed connect time-out

--- utils/helpers/wallet/switchNetwork.ts
+++ utils/helpers/wallet/switchNetwork.ts
@@ -16,21 +16,6 @@ const getCurrentNetwork = async (): Promise<number | null> => {
     }
 };
 
-function withTimeout<T>(promise: Promise<T>, ms: number): Promise<T> {
-    return new Promise((resolve, reject) => {
-        const timer = setTimeout(() => reject(new Error("Network switch timed out")), ms);
-        promise
-            .then((value) => {
-                clearTimeout(timer);
-                resolve(value);
-            })
-            .catch((err) => {
-                clearTimeout(timer);
-                reject(err);
-            });
-    });
-}
-
 // Network switch function
 const changeNetwork = async (networkName: string): Promise<void> => {
 
